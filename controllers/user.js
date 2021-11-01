0109f7224ce7e4d4382166f152f8d8c461d6a08e
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-01 18:18:33 -0400

    Add dbErrorHandler file to send error in a more understandable way

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const {errorHandler} = require('../helpers/dbErrorHandler');
 
 exports.signup = (req, res) => {
   const user = new User(req.body);
@@ -6,9 +7,12 @@ exports.signup = (req, res) => {
   user.save((err, user)=> {
     if(err) {
       return res.status(400).json({
-        err
+        err: errorHandler(err)
       });
     }
+
+    user.salt = undefined;
+    user.hash_password = undefined;
 
     res.json({
       user
