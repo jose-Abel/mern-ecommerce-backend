7ba6543dc4a8db1c830f697cb1c74ddc4c65296c
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-01 17:47:03 -0400

    Create the virtual field for the hashed password

--- models/user.js
+++ models/user.js
@@ -36,3 +36,34 @@ const userSchema = mongoose.Schema({
   }
 }, {timestamps: true});
 
+
+
+// virtual field
+userSchema.virtual('password')
+.set(function(password) {
+  this._password = password;
+  this.salt = uuidv1();
+  this.hashed_password = this.encryptPassword(password);
+})
+.get(function(){
+  return this._password;
+});
+
+userSchema.methods = {
+  encryptPassword: function(password) {
+    if(!password) return '';
+
+    try {
+      return crypto.createHmac('sha1', this.salt)
+      .update(password)
+      .digest('hex');
+
+    }catch(err) {
+      return "";
+    }
+  } 
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
