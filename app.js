511f394fdbe5c10500ae92981e7341cb90de39e4
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-07 14:56:00 -0400

    Create the model, route, controller for the category resource

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ require('dotenv').config();
 // import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+
 
 // app
 const app = express();
@@ -30,6 +32,7 @@ app.use(expressValidator());
 // routes middlewares
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
 
 
 const port = process.env.PORT || 8000;
---
+++ models/category.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');;
+
+const categorySchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    maxlength: 32
+  }
+}, {timestamps: true});
+
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
