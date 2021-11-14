8f6ab4b28452826751e52c2536ec6a24f914b50e
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-14 15:48:19 -0400

    Create the GET product controller and the productById param middleware to populate the product when the id is in the parameters of the request

--- controllers/product.js
+++ controllers/product.js
@@ -4,24 +4,59 @@ const _ = require('lodash');
 const Product = require('../models/product');
 const {errorHandler} = require('../helpers/dbErrorHandler');
 
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({
+        error: "Product not found"
+      })
+    }
+
+    req.product = product;
+
+    next();
+  });
+}
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+
+  return res.status(200).json(req.product);
+}
+
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
 
-  form.keepExtensions = true;
+  form.keepExtensions = false;
 
   form.parse(req, (err, fields, files) => {
     if(err) {
       return res.status(400).json({
         error: 'Image could not be uploaded'
-      })
+      });
+    }
+
+    // Check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (!name || !description || !price || !category || !quantity || !shipping) {
+      return res.status(400).json({
+        error: 'All fields are required'
+      });
     }
 
     let product = new Product(fields);
 
     if(files.photo) {
-      product.photo.data = fs.readFileSync(files.photo.path);
+      if(files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1MB in size'
+        });
+      }
 
-      product.photo.contentType = files.photo.type;
+      product.photo.data = fs.readFileSync(files.photo.filepath);
+  
+      product.photo.contentType = files.photo.mimetype;
     }
 
     product.save((err, result) => {
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { create } = require("../controllers/product");
+const { create, productById, read } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
+
+
+router.get('/product/:productId', read);
 
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 
 router.param("userId", userById);
-
+router.param("productId", productById);
 
 module.exports = router;
