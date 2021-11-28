c608a3c09ab7a4830a7c80d95dd6d22813f6048b
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-11-28 13:32:23 -0400

    Create the delete and update routes and methods controllers

--- controllers/product.js
+++ controllers/product.js
@@ -3,6 +3,7 @@ const formidable = require('formidable');
 const _ = require('lodash');
 const Product = require('../models/product');
 const {errorHandler} = require('../helpers/dbErrorHandler');
+const { response } = require('express');
 
 exports.productById = (req, res, next, id) => {
   Product.findById(id).exec((err, product) => {
@@ -46,6 +47,70 @@ exports.create = (req, res) => {
     }
 
     let product = new Product(fields);
+
+    if(files.photo) {
+      if(files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1MB in size'
+        });
+      }
+
+      product.photo.data = fs.readFileSync(files.photo.filepath);
+  
+      product.photo.contentType = files.photo.mimetype;
+    }
+
+    product.save((err, result) => {
+      if(err) {
+        return res.status(400).json({
+          error: errorHandler(error)
+        });
+      }
+      res.json(result);
+    });
+  });
+}
+
+exports.remove = (req, res) => {
+  let product = req.product
+
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(error)
+      });
+    }
+
+    response.json({
+      message: "Product deleted successfully"
+    });
+  })
+}
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+
+  form.keepExtensions = false;
+
+  form.parse(req, (err, fields, files) => {
+    if(err) {
+      return res.status(400).json({
+        error: 'Image could not be uploaded'
+      });
+    }
+
+    // Check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+
+    if (!name || !description || !price || !category || !quantity || !shipping) {
+      return res.status(400).json({
+        error: 'All fields are required'
+      });
+    }
+
+    let product = req.product;
+
+    product = _.extend(product, fields);
 
     if(files.photo) {
       if(files.photo.size > 1000000) {
--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { create, productById, read } = require("../controllers/product");
+const { create, productById, read, remove, update } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
-
 
 router.get('/product/:productId', read);
 
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
+
+route.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
+
+route.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
 
 router.param("userId", userById);
 router.param("productId", productById);
