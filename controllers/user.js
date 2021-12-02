a73a7b59ee36c5df9ea6f0ce15458643f5dfc69d
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-12-02 10:48:48 -0400

    Create the photo productController method to get the pictures of the products. Create the read and update routes and userController methods

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,5 @@
 const User = require('../models/user');
 
-
 exports.userById = (req, res, next, id) => {
   User.findById(id).exec((err, user) => {
     if(err || !user) {
@@ -12,5 +11,26 @@ exports.userById = (req, res, next, id) => {
     req.profile = user;
 
     next();
+  });
+}
+
+exports.read = (req, res) => {
+  req.profile.hashed_password = undefined;
+  req.profile.salt = undefined;
+
+  return res.json(req.profile);
+}
+
+exports.update = (req, res) => {
+  User.findOneAndUpdate({_id: req.profile._id}, {$set: req.body}, {new: true}, (err, user) => {
+    if (err) {
+      return res.status(400).json({
+        error: "You are not authorized to perform this action"
+      })
+    }
+
+    user.hashed_password = undefined;
+    user.salt = undefined;
+    res.json(user);
   });
 }
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { userById } = require("../controllers/user");
+const { userById, read, update } = require("../controllers/user");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
@@ -8,6 +8,10 @@ router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
     user: req.profile
   });
 });
+
+router.get('/user/:userId', requireSignin, isAuth, read);
+
+router.put('/user/:userId', requireSignin, isAuth, update);
 
 router.param("userId", userById);
 
