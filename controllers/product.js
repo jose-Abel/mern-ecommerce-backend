1b5c35f7ef650f931cf4f7756e72f8210d6b859f
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-12-02 10:17:49 -0400

    Create the listBySearch route and method controller to filter the search comming from the React frontend

--- controllers/product.js
+++ controllers/product.js
@@ -173,7 +173,6 @@ exports.listRelated = (req, res) => {
 }
 
 exports.listCategories = (req, res) => {
-  // Get all the categories used in the Product model distinct to product 
   Product.distinct("category", {}, (err, categories) => {
     if (err) {
       return res.status(400).json({
@@ -184,3 +183,47 @@ exports.listCategories = (req, res) => {
     res.json(categories)
   });
 }
+
+exports.listBySearch = (req, res) => {
+  let order = req.body.order ? req.body.order : "desc";
+  let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+  let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+  let skip = parseInt(req.body.skip);
+  let findArgs = {};
+
+  // console.log(order, sortBy, limit, skip, req.body.filters);
+  // console.log("findArgs", findArgs);
+
+  for (let key in req.body.filters) {
+      if (req.body.filters[key].length > 0) {
+          if (key === "price") {
+              // gte -  greater than price [0-10]
+              // lte - less than
+              findArgs[key] = {
+                  $gte: req.body.filters[key][0],
+                  $lte: req.body.filters[key][1]
+              };
+          } else {
+              findArgs[key] = req.body.filters[key];
+          }
+      }
+  }
+
+  Product.find(findArgs)
+      .select("-photo")
+      .populate("category")
+      .sort([[sortBy, order]])
+      .skip(skip)
+      .limit(limit)
+      .exec((err, data) => {
+          if (err) {
+              return res.status(400).json({
+                  error: "Products not found"
+              });
+          }
+          res.json({
+              size: data.length,
+              data
+          });
+      });
+};
--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { create, productById, read, remove, update, list, listRelated, listCategories } = require("../controllers/product");
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -17,6 +17,10 @@ router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 
 router.get('/products/categories', listCategories);
+
+router.post("/products/by/search", listBySearch);
+
+router.get('/products/photo/:productId', photo);
 
 router.param("userId", userById);
 router.param("productId", productById);
